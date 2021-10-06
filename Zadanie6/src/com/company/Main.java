eff3dc8739835196c890da9cd8aca23c3312635d
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2021-10-06 17:25:58 +0200

    Add files via upload

---
+++ Zadanie6/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("  /////\n+''''''+\n (|00|)\n |  ^  |\n | ,-, |\n +---+");
+    }
+}
---
+++ projekt/src/niewiem.java
@@ -0,0 +1,42 @@
+public class niewiem {
+//    zadanie 1-----------------------
+//    public static void main(String[] args) {
+//        System.out.print(31+29+31);
+//    }
+//    zadanie 2-----------------------
+//    public static void main(String[] args) {
+//        int wynik =1;
+//        for(int i = 2; i<11;i++)wynik+=i;
+//        System.out.print(wynik);
+//    }
+//    zadanie 3------------------------
+//    public static void main(String[] args) {
+//        int wynik =1;
+//        for(int i = 2; i<11;i++)wynik*=i;
+//        System.out.print(wynik);
+//    }
+//    zadanie 4--------------------------
+//    public static void main(String[] args) {
+//        float wynik = 1000;
+//        for (int i = 0; i < 3; i++) {
+//            wynik *= 1.06;
+//            System.out.print(wynik+"\n");
+//        }
+//    }
+//    zadanie 5-------------------------
+//    public static void main(String[] args) {
+//        System.out.print("+----+\n");
+//        System.out.print("|Java|\n");
+//        System.out.print("+----+");
+//    }
+//    zadanie 6------------------------
+//    public static void main(String[] args){
+//        System.out.print("  /////\n" +
+//                " +\"\"\"\"\"+\n" +
+//                "(| o o |)\n" +
+//                "  | ^ |\n" +
+//                " | `-' |\n" +
+//                " +-----+");
+//    }
+
+}
