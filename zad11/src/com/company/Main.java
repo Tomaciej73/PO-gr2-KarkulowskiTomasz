8339dbb55db9fd8bda07b34f96bc5cd5765bda66
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2021-10-09 12:45:46 +0200

    Add files via upload

---
+++ zad11/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Lorem Ipsum is simply dummy text of the printing and typesetting industry. \nLorem Ipsum has been the industry's standard dummy text ever since the 1500s, \nwhen an unknown printer took a galley of type and scrambled it to make a type specimen book. \nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ");
+    }
+}
---
+++ zad9/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("  V_V\n ( ' ' ) |Hello|\n d - b\n | | | \n (__|__)");
+    }
+}
