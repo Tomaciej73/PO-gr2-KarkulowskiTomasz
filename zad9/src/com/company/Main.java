dd93f9e6732028e71e718d5da8e96e016764a307
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2021-12-15 22:13:08 +0100

    Delete zad9 directory

--- zad9/src/com/company/Main.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("  V_V\n ( ' ' ) |Hello|\n d - b\n | | | \n (__|__)");
-    }
-}
