6ca0b01accfddcb3165b14b167d04e48fe87997b
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2022-01-12 18:02:33 +0100

    Add files via upload

---
+++ lab10/src/pl/edu/uwm/po/lab10/Student.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+
+public class Student extends Osoba implements Cloneable, Comparable<Osoba>{
+    public Student(String naz, LocalDate dU, double sO) {
+        super(naz, dU);
+        this.sredniaOcen=sO;
+    }
+
+    private double sredniaOcen;
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName()+"["+this.getNazwisko()+", "+this.getDataUrodzenia().toString()+", "+this.sredniaOcen+"]";
+    }
+
+    @Override
+    public int compareTo(Osoba o) {
+        int last=super.compareTo((o));
+        if((o instanceof Student)&&(last==0)){
+            return -(int)Math.ceil(this.sredniaOcen-((Student) o).sredniaOcen);
+        }
+        return last;
+    }
+}
